8793ca690f17f160839b0c326f23d2cfdc006278
Patryk Maciak <maciakpatryk32@gmail.com>
2022-10-22 11:16:03 +0200

    null

--- java/OOP/school/ClassTeacher.java
+++
@@ -1,9 +0,0 @@
-// wychowawca
-public final class ClassTeacher extends Teacher{
-    // F U C K ..... if have any problems with the extends add the FINAL
-    // ToDo => dodac pole okreslajace jakie klasy dotyczny
-
-    public ClassTeacher(String name, int age, String subject) {
-        super(name, age, subject);
-    }
-}
