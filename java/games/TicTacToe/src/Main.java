8636268b245d68ea99aa19d39783859f3c7d03da
Patryk Maciak <maciakpatryk32@gmail.com>
2022-11-28 22:11:06 +0100

    tic tac toe

--- java/games/TicTacToe/src/Main.java
+++ java/games/TicTacToe/src/Main.java
@@ -9,46 +9,17 @@ public class Main {
 
         displayBoard(board);
 
-        System.out.println("Where would you like to play? (1-9)");
         Scanner scanner = new Scanner(System.in);
-        int userChoice = scanner.nextInt();
-
-        switch (userChoice) {
-            case 1:
-                board[0][0] = 'X';
-                break;
-            case 2:
-                board[0][1] = 'X';
-                break;
-            case 3:
-                board[0][2] = 'X';
-                break;
-            case 4:
-                board[1][0] = 'X';
-                break;
-            case 5:
-                board[1][1] = 'X';
-                break;
-            case 6:
-                board[1][2] = 'X';
-                break;
-            case 7:
-                board[2][0] = 'X';
-                break;
-            case 8:
-                board[2][1] = 'X';
-                break;
-            case 9:
-                board[2][2] = 'X';
-                break;
-            default:
-                System.out.println("without range :(");
-        }
+        playernTurn(board, scanner);
 
         displayBoard(board);
-
     }
 
+    private static void playernTurn(char board[][], Scanner scanner)
+    {
+        System.out.println("Where would you like to play? (1-9)");
+        int userChoice = scanner.nextInt();
+    }
 
     private static void displayBoard(char board[][]) {
         System.out.println(board[0][0] + "|" + board[0][1] + "|" + board[0][2]);
