28dd61b48ce92af395acc135438cb18ff67a807c
Patryk Maciak <maciakpatryk32@gmail.com>
2023-03-07 12:36:52 +0100

    schemat hornera

---
+++ cpp/schematHornera/schematHornera/schematHornera.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// w -> wspolczynniki
+int horner(vector<int> w, int stopien, int x)
+{
+    int wynik = w[0];
+    for (int i = 1; i <= stopien; i++)
+    {
+        wynik = wynik * x + w[i];
+    }
+    return wynik;
+}
+
+int hornerRekurencyjnie(vector<int> w, int stopien, int x)
+{
+    if (stopien == 0)
+        return w[0];
+    return x * hornerRekurencyjnie(w, stopien - 1, x);
+}
+
+int main()
+{
+    cout << "SCHEMAT HORNERA\n";
+
+    vector<int> wspolczynniki;
+    wspolczynniki.push_back(1);
+    wspolczynniki.push_back(3);
+    wspolczynniki.push_back(-6);
+
+    int stopien = 2;
+    int x = 2;
+
+    cout << "iteracyjnie: " << horner(wspolczynniki, stopien, x);
+    cout << endl << 
+        "rekurencyjnie1: " << hornerRekurencyjnie(wspolczynniki, stopien, x);
+}
