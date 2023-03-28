fe7e915ebbb56446b31d582652fd0e681375cf2c
Patryk Maciak <maciakpatryk32@gmail.com>
2023-03-28 11:45:02 +0200

    gotowane zadanie z haslem

--- c#/formularze_kartkowka/formularze_kartkowka/MainWindow.xaml.cs
+++ c#/formularze_kartkowka/formularze_kartkowka/MainWindow.xaml.cs
@@ -32,12 +32,61 @@ namespace formularze_kartkowka
                 !string.IsNullOrEmpty(pass_length.Text))
             {
                 MessageBox.Show("Dlugosc hasla: " + password_length);
-                String user_password = " ";
+                String user_password = "";
+
                 Random random = new Random();
-                const string lower_case = "";
+
+                const string lower_case = "abcdefgihijklmnoprstwuxyz";
+                const string upper_case = "ABCDEFGHIJKLMNOPRSTWUXYZ";
+                const string numbers = "0123456789";
+                const string s_chars = "!@#$%&?";
+                string all_chars = "";
+                int _char, checkbox_count = 0;
+
+                if(small_caps.IsChecked == true)
+                {
+                    all_chars += lower_case;
+                    _char = random.Next(0, lower_case.Length - 1);
+                    user_password += lower_case.Substring(_char, 1); 
+                    checkbox_count++;
+                }
+
+                if(upper_caps.IsChecked == true)
+                {
+                    all_chars += upper_case;
+                    _char = random.Next(0, upper_case.Length - 1);
+                    user_password += upper_case.Substring(_char, 1);
+                    checkbox_count++;
+                }
+
+                if(digits.IsChecked == true)
+                {
+                    all_chars += numbers;
+                    _char = random.Next(0, numbers.Length - 1);
+                    user_password += numbers.Substring(_char, 1);
+                    checkbox_count++;
+                }
+
+                if(special_chars.IsChecked == true)
+                {
+                    all_chars += s_chars;
+                    _char = random.Next(0, s_chars.Length - 1);
+                    user_password += s_chars.Substring(_char, 1);
+                    checkbox_count++;
+                }
+
+                if((password_length - checkbox_count) >= 0)
+                {
+                    for(int i=0; i<(password_length - checkbox_count); i++)
+                    {
+                        user_password += all_chars[random.Next(0, all_chars.Length)];
+                    }
+                }
+
+                MessageBox.Show("Wygenerowane haslo:" + user_password);
             }
             else
-            {
+            {   
                 MessageBox.Show("Wyplenij prawidlowo!");
             }
         }
