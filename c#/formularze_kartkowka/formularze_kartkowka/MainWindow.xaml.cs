6cba3b2f0d9f205a2760015ab92c2255bef6d07a
Patryk Maciak <maciakpatryk32@gmail.com>
2023-03-28 10:58:05 +0200

    formularze kartkowka c#

---
+++ c#/formularze_kartkowka/formularze_kartkowka/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace formularze_kartkowka
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void random_password_generator(object sender, RoutedEventArgs e)
+        {
+            if(int.TryParse(pass_length.Text, out int password_length) && 
+                password_length > 0 && 
+                !string.IsNullOrEmpty(pass_length.Text))
+            {
+                MessageBox.Show("Dlugosc hasla: " + password_length);
+                String user_password = " ";
+                Random random = new Random();
+                const string lower_case = "";
+            }
+            else
+            {
+                MessageBox.Show("Wyplenij prawidlowo!");
+            }
+        }
+    }
+}
