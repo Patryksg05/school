c3afd8ea01f7e2f8786535e5155d1f51381357be
Patryk Maciak <maciakpatryk32@gmail.com>
2023-03-28 12:13:48 +0200

    kartkowka grupa2 bardzo latwa

--- c#/formularze_kartkowka2/formularze_kartkowka2/MainWindow.xaml.cs
+++ c#/formularze_kartkowka2/formularze_kartkowka2/MainWindow.xaml.cs
@@ -24,5 +24,42 @@ namespace formularze_kartkowka2
         {
             InitializeComponent();
         }
+
+        private void generate_random_login(object sender, RoutedEventArgs e)
+        {
+            string first_name = name_box.Text;
+            string last_name = surname_box.Text;
+            string pass1 = pass1_box.Text;
+            string pass2 = pass2_box.Text;
+            string generate_user_login = "";
+            Random random = new Random();
+
+            if (sub_metod(first_name) != "za krotkie!" && (sub_metod(last_name) != "za krotkie!"))
+            {
+                generate_user_login += sub_metod(first_name);
+                generate_user_login += sub_metod(last_name);
+                generate_user_login += random.Next(0, 9);
+                generate_user_login += random.Next(0, 9);
+
+                if (pass1 == pass2)
+                    result_textBlock.Text = generate_user_login;
+                else
+                    MessageBox.Show("haslo musza byc jednakowe!");
+            }
+            else
+            {
+                if (sub_metod(first_name) == "za krotkie!")
+                    MessageBox.Show("imie jest za krotkie!");
+                else if (sub_metod(last_name) == "za krotkie!")
+                    MessageBox.Show("nazwisko jest za krotkie!");
+            }
+        }
+
+        private string sub_metod(string s)
+        {
+            if (s.Length <= 3)
+                return "za krotkie!";
+            return s.Substring(0, 3);
+        }
     }
 }
